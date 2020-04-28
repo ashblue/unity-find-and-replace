95139968e33fae838d46421be9fdfd8d62c2a43a
Ash Blue <ash@blueashes.com>
2020-04-27 20:49:38 -0600

    refactor: find window no longer crashes on recompile

--- Assets/Examples/Scripts/SearchExample.cs
+++ Assets/Examples/Scripts/SearchExample.cs
@@ -2,27 +2,32 @@ using System;
 using System.Collections.Generic;
 using CleverCrow.Fluid.FindAndReplace.Editors;
 using UnityEditor;
-using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace CleverCrow.Fluid.FindAndReplace.Examples {
     public class SearchResult : IFindResult {
+        private readonly Object _target;
         public string Text { get; }
-        public void Show () {
-            throw new NotImplementedException();
+
+        public SearchResult (string text, Object target) {
+            _target = target;
+            Text = text;
         }
 
-        public SearchResult (string text) {
-            Text = text;
+        public void Show () {
+            Selection.activeObject = _target;;
         }
     }
 
-    public class SearchExample {
+    public class SearchExample : FindReplaceWindowBase {
+        private static SearchExample _window;
+
         [MenuItem("Examples/Find Replace")]
         public static void ShowFindReplace () {
-            FindReplaceWindow.ShowWindow(GetFindResults);
+            ShowWindow<SearchExample>();
         }
 
-        private static IFindResult[] GetFindResults (Func<string, bool> IsValid) {
+        protected override IFindResult[] GetFindResults (Func<string, bool> IsValid) {
             var results = new List<IFindResult>();
 
             var textIDs = AssetDatabase.FindAssets("t:TextExample");
@@ -31,7 +36,7 @@ namespace CleverCrow.Fluid.FindAndReplace.Examples {
                 var textExample = AssetDatabase.LoadAssetAtPath<TextExample>(path);
 
                 if (IsValid(textExample.text)) {
-                    results.Add(new SearchResult(textExample.text));
+                    results.Add(new SearchResult(textExample.text, textExample));
                 }
             }
 
